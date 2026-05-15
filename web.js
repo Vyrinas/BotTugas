7b63c00c53bc70cd2f187712808eef8ae648cc62
Vyrina <105841115825@student.unismuh.id>
2026-05-15 21:52:39 +0700

    Move static files to root and simplify Vercel config

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // Middleware untuk memastikan koneksi DB
 app.use(async (req, res, next) => {
