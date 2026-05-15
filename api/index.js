740f73cd5f38d5b9b6e5c81d9a8531fdfba26ace
Vyrina <105841115825@student.unismuh.id>
2026-05-15 21:58:54 +0700

    Move public folder inside api to ensure Vercel bundling

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware untuk memastikan koneksi DB
 app.use(async (req, res, next) => {
@@ -18,7 +18,7 @@ app.use(async (req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/health', (req, res) => res.status(200).send('OK'));
